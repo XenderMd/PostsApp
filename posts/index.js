d55495317a4fa2f254f9def1433e249eff38d3fe
DStavilaNI <denis.stavila@ni.com>
2022-06-10 18:09:58 +0300

    Implementing a comments service

--- posts/index.js
+++ posts/index.js
@@ -16,6 +16,7 @@ app.post('/posts', (req, res)=>{
     const id = randomBytes(4).toString('hex');
     const {title}=req.body;
     posts[id]={id,title};
+    res.status(201).send(posts[id]);
 
 });
 
