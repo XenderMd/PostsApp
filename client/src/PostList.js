71694796c19c9faa845f23c6d8674fecbfc82279
XenderMd <denis.stavilamd@gmail.com>
2022-06-14 14:24:44 +0300

    code cleanup

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,35 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const PostList = ()=>{
+const PostList = () => {
+  const [posts, setPosts] = useState({});
 
-    const [posts, setPosts]=useState({});
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
 
-    const fetchPosts=async()=>{
+    setPosts(res.data);
+  };
 
-        const res = await axios.get('http://localhost:4000/posts');
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-        setPosts(res.data);
-    };
-
-    useEffect(()=>{
-        fetchPosts();
-    },[]);
-
-    const renderedPosts=Object.values(posts).map(post=>{
-        return <div className='card' 
-                    style={{width:'30%', marginBottom:'20px'}}
-                    key={post.id}>
-                    <div className='card-body'>
-                        <h3>{post.title}</h3>
-                    </div>
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
         </div>
-    })
+      </div>
+    );
+  });
 
-    return(<div 
-                className='d-flex flex-row flex-wrap justify-content-between'>
-                {renderedPosts}
-            </div>);
-}
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+};
 
 export default PostList;
